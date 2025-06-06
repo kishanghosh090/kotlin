d4905561c7bc484e5d836106d8951302651f8332
kishanghosh090 <kishanghosh090@gmail.com>
2025-06-06 23:47:29 +0530

    kotlin learing

---
+++ variables/src/main/kotlin/Arrays.kt
@@ -0,0 +1,22 @@
+fun main(){
+    val myNumbers = intArrayOf(1,2,3,4,5)
+    val myChar = charArrayOf('a','b','c')
+    val myString = arrayOf("kishan","suresh","jose")
+    val myBoolean = booleanArrayOf(true,false,true)
+    val myFloat = floatArrayOf(1.2F,2.3F,3.4F)
+    val myDouble = doubleArrayOf(1.2,2.3,3.4)
+    val myLong = longArrayOf(1,2,3,4,5)
+
+    println(myNumbers[0])
+    println(myChar[0])
+    println(myString[0])
+    println(myBoolean[0])
+    println(myFloat[0])
+    println(myDouble[0])
+    println(myLong[0])
+
+    val myArr = arrayOf(1,"kishan",true)
+    println(myArr[0])
+    println(myArr[1])
+    println(myArr[2])
+}
